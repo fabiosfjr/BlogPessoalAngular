ba7f01b56ffd3ef454a75f4e988b309a31df4c39
fabiosfjr <fabio_junior1994@hotmail.com>
2022-03-31 02:32:28 -0300

    Criando projeto Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', //caminho do seletor
+  templateUrl: './app.component.html', //template
+  styleUrls: ['./app.component.css'] //estilo CSS
+})
+export class AppComponent {
+  title = 'BlogPessoal';
+}
